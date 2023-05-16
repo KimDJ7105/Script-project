58cc892d177925101cf6d19944172b072840d0c1
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2023-05-16 14:34:39 +0900

    Update main.py
    notebook 7개 추가 완료

--- main.py
+++ main.py
@@ -11,12 +11,20 @@ class MainGUI :
         nb = tkinter.ttk.Notebook(window,width=800,height=600)
         nb.pack()
         
-        framek = Frame(window) #요양시설 검색할 탭
-        nb.add(framek,text='요양시설')
+        frame1 = Frame(window) #요양시설 검색할 탭
+        nb.add(frame1,text='요양시설')
         frame2 = Frame(window) #전문병원 검색할 탭
         nb.add(frame2,text='전문병원')
         frame3 = Frame(window) #여가복지시설(경로당) 검색 탭
         nb.add(frame3,text='여가복지시설')
+        frame4 = Frame(window) #의료복지시설 검색 탭
+        nb.add(frame4,text='의료복지시설')
+        frame5 = Frame(window) #일자리지원기관 검색 탭
+        nb.add(frame5,text='일자리지원기관')
+        frame6 = Frame(window) #주거복지시설 검색 탭
+        nb.add(frame6,text='주거복지시설')
+        frame7 = Frame(window) #즐겨찾기 탭
+        nb.add(frame7,text='즐겨찾기')
         
         window.mainloop()
 
