620413e384a5e2a986806523aabbfd31de87ce46
papyrio <130471656+papyrio@users.noreply.github.com>
2023-05-16 14:26:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ class MainGUI :
         nb = tkinter.ttk.Notebook(window,width=800,height=600)
         nb.pack()
         
-        frame1 = Frame(window) #요양시설 검색할 탭
-        nb.add(frame1,text='요양시설')
+        framek = Frame(window) #요양시설 검색할 탭
+        nb.add(framek,text='요양시설')
         frame2 = Frame(window) #전문병원 검색할 탭
         nb.add(frame2,text='전문병원')
         
