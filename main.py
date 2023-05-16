b8310c6dfadd57ac88aad0b27103621baa1c615b
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2023-05-16 14:23:16 +0900

    main파일 수정
    main파일에 tkinter와 간단한 notebook 기능 추가

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from tkinter import *
+import tkinter.ttk
+
 
 class MainGUI :
     def __init__(self) :
-        pass
+        window = Tk()
+        window.title("노인통합 서비스")
+        window.geometry("800x600")
+        
+        nb = tkinter.ttk.Notebook(window,width=800,height=600)
+        nb.pack()
+        
+        frame1 = Frame(window) #요양시설 검색할 탭
+        nb.add(frame1,text='요양시설')
+        frame2 = Frame(window) #전문병원 검색할 탭
+        nb.add(frame2,text='전문병원')
+        
+        window.mainloop()
+
 
 MainGUI()
