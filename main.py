6991af061a2839e171cd2007852792dfce33e653
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2023-05-20 14:54:02 +0900

    Revert "Update main.py"
    This reverts commit 03d4098483a6f43ac63f976e8fc5fa38b6bcebbd.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from tkinter import *
-import tkinter.messagebox
 import tkinter.ttk
 
 
-<<<<<<< Updated upstream
 class MainGUI :
     def searchButton(self):
         self.e = {}  # 딕셔너리를 초기화
@@ -12,9 +10,6 @@ class MainGUI :
             self.e[i].place(x=10, y=10)
             Button(self.frame[i], text='검색').place(x=150, y=10)
 
-=======
-class MainGUI:
->>>>>>> Stashed changes
     def __init__(self) :
         window = Tk()
         window.title("노인통합 서비스")
@@ -23,7 +18,6 @@ class MainGUI:
         nb = tkinter.ttk.Notebook(window,width=800,height=600)
         nb.pack()
         
-<<<<<<< Updated upstream
         self.frame1 = Frame(window) #요양시설 검색할 탭
         nb.add(self.frame1,text='요양시설')
         self.frame2 = Frame(window) #전문병원 검색할 탭
@@ -43,22 +37,6 @@ class MainGUI:
 
 
 
-=======
-        frame1 = Frame(window) #요양시설 검색할 탭
-        nb.add(frame1,text='요양시설')
-        frame2 = Frame(window) #전문병원 검색할 탭
-        nb.add(frame2,text='전문병원')
-        frame3 = Frame(window) #여가복지시설(경로당) 검색 탭
-        nb.add(frame3,text='여가복지시설')
-        frame4 = Frame(window) #의료복지시설 검색 탭
-        nb.add(frame4,text='의료복지시설')
-        frame5 = Frame(window) #일자리지원기관 검색 탭
-        nb.add(frame5,text='일자리지원기관')
-        frame6 = Frame(window) #주거복지시설 검색 탭
-        nb.add(frame6,text='주거복지시설')
-        frame7 = Frame(window) #즐겨찾기 탭
-        nb.add(frame7,text='즐겨찾기')
->>>>>>> Stashed changes
 
         window.mainloop()
 
