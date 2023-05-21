8b926428117785516e02333882252f45785aecf3
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2023-05-21 18:32:30 +0900

    Update main.py
    오픈 데이터 불러오기 및 검색 구현/
    파싱, 스크롤 구현 필요

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import tkinter.ttk
 import json
 
+key = 'fc79933d2b8f4ef3bdb6190a73ae8314'
 
 class MainGUI:
     def search(self, tab_index):
@@ -11,113 +12,60 @@ class MainGUI:
 
         if tab_index == 0:
             # 요양시설 검색
-            url = f''
-            params = {'SIGUN_NM': search_query}
-            response = requests.get(url)
-            data = json.loads(response.text)
-
-            # 검색 결과를 self.search_results에 저장
-            self.search_results = data["SIGUN_NM"]
-
-            # 검색 결과를 리스트 박스에 추가.
-            result_list = Listbox(self.framelist[tab_index], width=60, height=20)
-            result_list.place(x=0, y=200)
-
-            for result in self.search_results:
-                result_list.insert(END, SIGUN_NM)
+            url = f'https://openapi.gg.go.kr/OldPersonRecuperationFacility'
+            params = {'KEY' : key,'Type' : 'xml', 'pIndex' : 1, 'pSize' : 100, 'SIGUN_NM': search_query}
+            response = requests.get(url, params=params)
+            
+            self.lboxlist[tab_index].insert(END,response.text)
 
         elif tab_index == 1:
             # 전문병원 검색
             url = f'https://openapi.gg.go.kr/OldPersonSpecialityHospital'
-            params = {'SIGUN_NM': search_query}
+            params = {'KEY' : key,'Type' : 'xml', 'pIndex' : 1, 'pSize' : 100, 'SIGUN_NM': search_query}
             response = requests.get(url, params=params)
-            data = json.loads(response.text)
+            #data = json.loads(response.text)
 
-            # 검색 결과를 self.search_results에 저장
-            self.search_results = []
-            if 'row' in data:
-                self.search_results = [
-                    f"{item['HOSPTL_NM']}, {item['TREAT_SBJECT_CNT']}"
-                    for item in data['row']
-                ]
-            else:
-                self.search_results = ['검색 결과가 없습니다.']
+            # # 검색 결과를 self.search_results에 저장
+            # self.search_results = []
+            # if 'row' in data:
+            #     self.search_results = [
+            #         f"{item['HOSPTL_NM']}, {item['TREAT_SBJECT_CNT']}"
+            #         for item in data['row']
+            #     ]
+            # else:
+            #     self.search_results = ['검색 결과가 없습니다.']
 
-            # 검색 결과를 리스트 박스에 추가
-            result_list = Listbox(self.framelist[tab_index], width=60, height=20)
-            result_list.place(x=0, y=200)
+            # # 검색 결과를 리스트 박스에 추가
+            # result_list = Listbox(self.framelist[tab_index], width=60, height=20)
+            # result_list.place(x=0, y=200)
 
-            for result in self.search_results:
-                result_list.insert(END, result)
-
-
+            # for result in self.search_results:
+            #     result_list.insert(END, result)
+            self.lboxlist[tab_index].insert(END,response.text)
+        
         elif tab_index == 2:
             # 여가복지시설 검색
             url = f''
             params = {'SIGUN_NM': search_query}
             response = requests.get(url)
-            data = json.loads(response.text)
-
-            # 검색 결과를 self.search_results에 저장
-            self.search_results = data['']
-
-            # 검색 결과를 리스트 박스에 추가.
-            result_list = Listbox(self.framelist[tab_index], width=60, height=20)
-            result_list.place(x=0, y=200)
-
-            for result in self.search_results:
-                result_list.insert(END, SIGUN_NM)
 
         elif tab_index == 3:
             # 의료복지시설 검색
             url = f''
             params = {'SIGUN_NM': search_query}
             response = requests.get(url)
-            data = json.loads(response.text)
-
-            # 검색 결과를 self.search_results에 저장
-            self.search_results = data['']
-
-            # 검색 결과를 리스트 박스에 추가.
-            result_list = Listbox(self.framelist[tab_index], width=60, height=20)
-            result_list.place(x=0, y=200)
-
-            for result in self.search_results:
-                result_list.insert(END, SIGUN_NM)
 
         elif tab_index == 4:
             # 일자리지원기관 검색
             url = f''
             params = {'SIGUN_NM': search_query}
             response = requests.get(url)
-            data = json.loads(response.text)
-
-            # 검색 결과를 self.search_results에 저장
-            self.search_results = data['']
-
-            # 검색 결과를 리스트 박스에 추가.
-            result_list = Listbox(self.framelist[tab_index], width=60, height=20)
-            result_list.place(x=0, y=200)
-
-            for result in self.search_results:
-                result_list.insert(END, SIGUN_NM)
 
         elif tab_index == 5:
             # 주거복지시설 검색
             url = f''
             params = {'SIGUN_NM': search_query}
             response = requests.get(url)
-            data = json.loads(response.text)
-
-            # 검색 결과를 self.search_results에 저장
-            self.search_results = data['']
-
-            # 검색 결과를 리스트 박스에 추가.
-            result_list = Listbox(self.framelist[tab_index], width=60, height=20)
-            result_list.place(x=0, y=200)
-
-            for result in self.search_results:
-                result_list.insert(END, SIGUN_NM)
 
     def __init__(self):
         window = Tk()
@@ -140,22 +88,23 @@ class MainGUI:
         nb.add(self.framelist[5], text='주거복지시설')
         nb.add(self.framelist[6], text='즐겨찾기')
 
+        Button(self.framelist[0], text='검색', command=lambda: self.search(0)).place(x=150, y=10)
+        Button(self.framelist[1], text='검색', command=lambda: self.search(1)).place(x=150, y=10)
+        Button(self.framelist[2], text='검색', command=lambda: self.search(2)).place(x=150, y=10)
+        Button(self.framelist[3], text='검색', command=lambda: self.search(3)).place(x=150, y=10)
+        Button(self.framelist[4], text='검색', command=lambda: self.search(4)).place(x=150, y=10)
+        Button(self.framelist[5], text='검색', command=lambda: self.search(5)).place(x=150, y=10)
+        Button(self.framelist[6], text='검색', command=lambda: self.search(6)).place(x=150, y=10)
+        
         self.entrylist = []
+        self.lboxlist = [] #리스트 박스가 담길 리스트
 
         for i in range(7):
             self.entrylist.append(Entry(self.framelist[i], width=19))
             self.entrylist[i].place(x=10, y=10)
-
-            Button(self.framelist[i], text='검색', command=lambda: self.search(i)).place(x=150, y=10)
-
-        # 요양시설 탭에 필요한 구성품들
-
-        hospital_list = Listbox(self.framelist[0], width=60, height=20)
-        hospital_list.place(x=0, y=200)
-
-        # 전문병원 탭에 필요한 구성품들
-        result_list = Listbox(self.framelist[1], width=60, height=20)
-        result_list.place(x=0, y=200)
+            
+            self.lboxlist.append(Listbox(self.framelist[i],width=60,height=10))
+            self.lboxlist[i].place(x=5,y=80)
 
         window.mainloop()
 
