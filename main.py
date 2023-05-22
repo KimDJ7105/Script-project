aeecd7bd3794dd01a1d2a63476a3abf649713b4b
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2023-05-22 13:56:59 +0900

    Update main.py
    의료복지, 여가복지 검색기능 추가

--- main.py
+++ main.py
@@ -67,27 +67,71 @@ class MainGUI:
         
         elif tab_index == 2:
             # 여가복지시설 검색
-            url = f''
-            params = {'SIGUN_NM': search_query}
+            url = f'https://openapi.gg.go.kr/SenircentFaclt'
+            params = {'KEY' : key,'Type' : 'xml', 'pIndex' : 1, 'pSize' : 100, 'SIGUN_NM': search_query}
             response = requests.get(url)
+            
+            root = ET.fromstring(response.text)
+            
+            for item in root.iter('row'):
+                name = item.findtext('FACLT_NM') #시설명
+                capa = item.findtext('USE_MBER_CNT') #이용중인 회원 수
+                num = item.findtext('DETAIL_TELNO') #전화번호
+                depart = item.findtext('JURISD_CHARGE_DEPT_NM') #관할 담당 부서
+                
+                self.lboxlist[tab_index].insert(END,"시설명 : " + name + " 전화번호 : " + num + " 회원 수 : " + capa + " 관할 부서 : " + depart)
 
         elif tab_index == 3:
             # 의료복지시설 검색
-            url = f''
-            params = {'SIGUN_NM': search_query}
+            url = f'https://openapi.gg.go.kr/OldpsnMedcareWelfac'
+            params = {'KEY' : key,'Type' : 'xml', 'pIndex' : 1, 'pSize' : 100, 'SIGUN_NM': search_query}
             response = requests.get(url)
+            
+            root = ET.fromstring(response.text)
+            
+            for item in root.iter('row'):
+                name = item.findtext('FACLT_NM') #시설명
+                facl_type = item.findtext('FACLT_KIND_NM') #시설 종류
+                qual = item.findtext('LNGTR_RECPER_APPONT_INST_YN_NM') #장기요양지정 여부
+                capa = item.findtext('ENTRNC_PSN_CAPA') #입소 정원
+                
+                self.lboxlist[tab_index].insert(END,"시설 종류 : " + facl_type + " 시설명 : " + name + " 장기요양지정 여부 : " + qual + " 입소 정원 : " + capa)
 
         elif tab_index == 4:
             # 일자리지원기관 검색
             url = f''
-            params = {'SIGUN_NM': search_query}
+            params ={'KEY' : key,'Type' : 'xml', 'pIndex' : 1, 'pSize' : 100, 'SIGUN_NM': search_query}
             response = requests.get(url)
+            
+            root = ET.fromstring(response.text)
+            
+            for item in root.iter('row'):
+                name = item.findtext('HOSPTL_NM') #병원명
+                capa = item.findtext('SICKBD_CNT') #병상 수
+                qual = item.findtext('TREAT_SBJECT_CNT') #진료 과목 수
+                area = item.findtext('TREAT_SBJECT_DTLS') #진료 과목 내용
+                
+                #listbox에 검색 결과 출력, 추후 출력 내용 변경 필요
+                #위치 정보도 있음, 홈페이지 주소도.
+                self.lboxlist[tab_index].insert(END,"병원명 : " + name + "병상 수 : " + capa  + "진료 과목 내용 : (" + qual + "개), " + area)
 
         elif tab_index == 5:
             # 주거복지시설 검색
             url = f''
-            params = {'SIGUN_NM': search_query}
+            params = {'KEY' : key,'Type' : 'xml', 'pIndex' : 1, 'pSize' : 100, 'SIGUN_NM': search_query}
             response = requests.get(url)
+            
+            root = ET.fromstring(response.text)
+            
+            for item in root.iter('row'):
+                name = item.findtext('HOSPTL_NM') #병원명
+                capa = item.findtext('SICKBD_CNT') #병상 수
+                qual = item.findtext('TREAT_SBJECT_CNT') #진료 과목 수
+                area = item.findtext('TREAT_SBJECT_DTLS') #진료 과목 내용
+                
+                #listbox에 검색 결과 출력, 추후 출력 내용 변경 필요
+                #위치 정보도 있음, 홈페이지 주소도.
+                self.lboxlist[tab_index].insert(END,"병원명 : " + name + "병상 수 : " + capa  + "진료 과목 내용 : (" + qual + "개), " + area)
 
     def __init__(self):
         window = Tk()
